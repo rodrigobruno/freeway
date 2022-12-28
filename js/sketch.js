f96b692525ca00cb9318567b790e013893a09596
Rodrigo Bruno <rodrigo@rodrigobruno.com>
2022-12-28 11:14:39 -0300

    Adicionado arquivos básicos

---
+++ js/sketch.js
@@ -0,0 +1,18 @@
+const wCanva = 600;
+const hCanva = 400;
+
+function preload() {
+
+}
+
+function setup() {
+
+    createCanvas(wCanva, hCanva);
+
+}
+
+function draw() {
+
+    background(0);
+
+}
